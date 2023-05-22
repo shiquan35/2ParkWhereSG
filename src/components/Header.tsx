8de3a06356cc091e40ea9e36377ce305ddc74b56
Shi Quan <ongshiquan35@gmail.com>
2023-05-23 01:45:14 +0800

    added map and header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./styles.css";
+// export interface IAppProps {
+// }
+
+// export function Header (props: IAppProps) {
+export function Header() {
+  return (
+    <>
+      <div className="header">This is header</div>
+    </>
+  );
+}
---
+++ src/components/Map.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./styles.css";
+// export interface IAppProps {
+// }
+
+// export function Header (props: IAppProps) {
+export function Map() {
+  return (
+    <>
+      <div className="map">Map goes here</div>
+    </>
+  );
+}
