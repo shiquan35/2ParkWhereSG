c9e4411cdc922dfc2c530d1a3bb229a9fe58b406
Shi Quan <ongshiquan35@gmail.com>
2023-05-24 22:12:46 +0800

    added screenSize to logic

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -117,7 +117,7 @@ export function Map() {
 
   return (
     <>
-      {currentLocation && (
+      {currentLocation && screenSize && (
         <ReactMapGL
           style={{
             width: `${screenSize.width}px`,
